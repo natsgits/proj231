71265ce1496ade593492992280c991cd1cf586f7
Natesan <natesh2311@gmail.com>
2024-08-26 03:36:42 +0530

    adding scripts and more elements for functionality

--- scripts.js
+++ scripts.js
@@ -15,6 +15,7 @@ const btn7 = document.querySelector('button#seven');
 const btn8 = document.querySelector('button#eight');
 const btn9 = document.querySelector('button#nine');
 const btn10 = document.querySelector('button#zero');
+cons
 
 const btnop1 = document.querySelector("button#add");
 const btnop2 = document.querySelector("button#subtract");
@@ -22,85 +23,125 @@ const btnop3 = document.querySelector("button#multiply");
 const btnop4 = document.querySelector("button#divide");
 const btnop5 = document.querySelector("button#remainder");
 const btnop6 = document.querySelector("button#equals")
+const btnstart = document.querySelector("button#start");
 
 //functions
-var counter = 0;
+
+var clickercount= 0;
+var arr=[];
+
+
 function flowcontrol(){
-    msgs = ["enetr first number", "enter second number", "enter operator" , ]
+    let counter = 0;
+    let e1=0;
+    let e2=0;
+    let res=0;
+    let regex = /[0-9]/g;
+    let regexop = /[^0-9]/g;
+    msgs = ["enter first number and click next", "enter second number", "enter operator", "result = " ]
     let flow = 4;
-    counter++;
-    if(init==1){
-        disp4.innerText = ""
-    }
+    
+        if(counter==1){
+            disp4.innerText = msgs[0];
+            if(arr.slice(-1).match(regexop)!==null){
+                e1 = Number(arr.slice(0,arr.length-1).join(''));
+                counter++; 
+            }
+        }
+        if(counter==2){
+            disp4.innerText = msgs[1];
+        }
+
+    
+    
+    
 }
 
 //adding event-listeners
 
+
 btn1.addEventListener('click',()=>{
     arr.push(1);
+    clickercount++;
 })
 
 btn2.addEventListener('click',()=>{
     arr.push(2);
+    clickercount++;
 })
 
 btn3.addEventListener('click',()=>{
     arr.push(3);
+    clickercount++;
 })
 
 btn4.addEventListener('click',()=>{
     arr.push(4);
+    clickercount++;
 })
 
 btn5.addEventListener('click',()=>{
     arr.push(5);
+    clickercount++;
 })
 
 btn6.addEventListener('click',()=>{
     arr.push(6);
+    clickercount++;
 })
 
 btn7.addEventListener('click',()=>{
     arr.push(7);
+    clickercount++;
 })
 
 btn8.addEventListener('click',()=>{
     arr.push(8);
+    clickercount++;
 })
 
 btn9.addEventListener('click',()=>{
     arr.push(9);
+    clickercount++;
 })
 
 btn10.addEventListener('click',()=>{
     arr.push(0);
+    clickercount++;
 })
 
 btnop1.addEventListener('click',()=>{
     arr1.push('+');
+    clickercount++;
 })
 
 btnop2.addEventListener('click',()=>{
     arr1.push('-');
+    clickercount++;
 })
 
 btnop3.addEventListener('click',()=>{
     arr1.push('*');
+    clickercount++;
 })
 
 btnop4.addEventListener('click',()=>{
    arr1.push('/');
+   clickercount++;
 })
 
 btnop5.addEventListener('click',()=>{
     arr1.push('%');
+    clickercount++;
 })
 
 btnop6.addEventListener('click',()=>{
     operation();
 })
 
-
+btnstart.addEventListener('click',()=>{
+    flowcontrol();
+})
 
 
 
